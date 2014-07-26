40752a8930ddd0bf36cfd08b9debf687de1e112c
NipunDesai <nipun@promactinfo.com>
2014-07-26 17:35:37 +0530

    create a directive that simply replaces its contents with a static template

---
+++ DemoApplication/DemoApplication/app/admin/admin.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'admin';
+    angular.module('app').controller(controllerId, ['$scope','common', admin]);
+
+    function admin($scope, common) {
+        var _this = this;
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+        $scope.emailvalid = /^[a-z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-z0-9-]+(\.[a-z0-9-]+)*$/i;
+
+        $scope.users = [
+        {
+            name:'nipun',address:'vadodara',emailid:'nipun@promactinfo.com'
+        },
+        {
+            name: 'rushi', address: 'vadodara', emailid: 'rushi@promactinfo.com'
+        },
+            {
+                name: 'riya', address: 'vadodara', emailid: 'riya@promactinfo.com'
+            }
+        ];
+
+        $scope.save = function() {
+
+            $scope.users.push($scope.newuser);
+            $scope.newuser = {};
+        };
+
+
+        activate();
+
+        function activate() {
+            common.activateController([], controllerId)
+                .then(function () { log('Activated Admin View'); });
+        }
+    }
+})();
